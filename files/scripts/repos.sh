462be1044132e97d19fad31dfeaa6cdb16d773b6
ammix <maxim@ammix.dev>
2025-08-10 16:11:22 +0200

    chore: restructure

---
+++ files/scripts/repos.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+echo "Setting repository configurations..."
+
+dnf5 config-manager --setopt=rpmfusion-free.priority=90
+dnf5 config-manager --setopt=rpmfusion-free-updates.priority=90
+dnf5 config-manager --setopt=rpmfusion-nonfree.priority=90
+dnf5 config-manager --setopt=rpmfusion-nonfree-updates.priority=90
+dnf5 config-manager --setopt=rpmfusion-nonfree-tainted.priority=90
+
+echo "Successfully configured repositories."
