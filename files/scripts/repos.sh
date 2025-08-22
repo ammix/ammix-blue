d05a8c16136140664756efd86d2603d12be115ef
ammix <maxim@ammix.dev>
2025-08-23 01:41:59 +0200

    try build without rpmfusion

--- files/scripts/repos.sh
+++ files/scripts/repos.sh
@@ -5,7 +5,7 @@ echo "Setting repository configurations..."
 dnf5 -y config-manager setopt "*terra*".priority=1
 dnf5 -y config-manager setopt "terra-mesa".enabled=true
 dnf5 -y config-manager setopt "terra-nvidia".enabled=false
-dnf5 -y config-manager setopt "*rpmfusion*".priority=5 "*rpmfusion*".exclude="mesa-*"
+# dnf5 -y config-manager setopt "*rpmfusion*".priority=5 "*rpmfusion*".exclude="mesa-*"
 dnf5 -y config-manager setopt "*fedora*".exclude="mesa-*"
 
 echo "Successfully configured repositories."
