5459b866df83040eb828501f2ca5ad240acf6d30
ammix <maxim@ammix.dev>
2025-08-23 15:07:24 +0200

    try ghostty nightly again

--- files/scripts/repos.sh
+++ files/scripts/repos.sh
@@ -5,7 +5,7 @@ echo "Setting repository configurations..."
 dnf5 -y config-manager setopt "*terra*".priority=1
 dnf5 -y config-manager setopt "terra-mesa".enabled=true
 dnf5 -y config-manager setopt "terra-nvidia".enabled=false
-dnf5 -y config-manager setopt "*terra*".exclude="gstreamer* nerd-fonts"
+dnf5 -y config-manager setopt "*terra*".exclude="gstreamer*"
 dnf5 -y config-manager setopt "*rpmfusion*".priority=5 "*rpmfusion*".exclude="mesa-* ffmpeg*"
 dnf5 -y config-manager setopt "*fedora*".exclude="mesa-*"
 
