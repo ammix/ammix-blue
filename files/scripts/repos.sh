373d49f824de49741ae5a6480d4588125673bff4
ammix <maxim@ammix.dev>
2025-08-31 12:37:20 +0200

    Revert "switch back to rpmfusion for the time"
    This reverts commit 2a5437b7bd8f27834810d2bff363221e26ee1878.

--- files/scripts/repos.sh
+++ files/scripts/repos.sh
@@ -2,11 +2,11 @@
 
 echo "Setting repository configurations..."
 
-dnf5 -y config-manager setopt "*terra*".priority=5
+dnf5 -y config-manager setopt "*terra*".priority=1
 dnf5 -y config-manager setopt "terra-mesa".enabled=true
 dnf5 -y config-manager setopt "terra-nvidia".enabled=false
-dnf5 -y config-manager setopt "*terra*".exclude="gstreamer* *firmware* ffmpeg*"
-dnf5 -y config-manager setopt "*rpmfusion*".priority=1 "*rpmfusion*".exclude="mesa-*"
+dnf5 -y config-manager setopt "*terra*".exclude="gstreamer* *firmware*"
+dnf5 -y config-manager setopt "*rpmfusion*".priority=5 "*rpmfusion*".exclude="mesa-* ffmpeg*"
 dnf5 -y config-manager setopt "*fedora*".exclude="mesa-*"
 
 echo "Successfully configured repositories."
